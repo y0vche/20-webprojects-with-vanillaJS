2b8d1fa68b015f3379753a47568ea0d3cbb4a9cc
yovche <70894119+y0vche@users.noreply.github.com>
2023-08-01 22:33:38 +0200

    first commit

---
+++ infinite-scrolls/script.js
@@ -0,0 +1,61 @@
+filter = document.getElementById('filter');
+postContainer = document.getElementById('posts-container');
+loading = document.querySelector('.loader');
+
+let limit = 5;
+let page = 1;
+
+async function getPosts() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  )
+
+  const data = await res.json();
+
+  return data;
+}
+
+// Show posts in DOM
+async function showPosts () {
+  const posts = await getPosts();
+
+  posts.forEach(post => {
+    const postEl = document.createElement('div');
+    postEl.classList.add('post');
+    postEl.innerHTML = `
+    <div class="number">${post.id}</div>
+    <div class="post-info">
+      <h2 class="post-title">${post.title}</h2>
+      <p class="post-body">${post.body}</p>
+    </div>
+    `
+
+    postContainer.appendChild(postEl);
+  });
+}
+
+// Show loader & fetch more posts
+function showLoading() {
+  loading.classList.add('show');
+
+  setTimeout(() => {
+    loading.classList.remove('show');
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300)
+
+  }, 1000);
+}
+
+// Show initial posts 
+showPosts();
+
+window.addEventListener('scroll', () => {
+  const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
